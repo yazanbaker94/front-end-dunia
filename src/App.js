a0bfefb4af4dbf963c35a3a0a4a66fc628bab3ee
Duniaalkilany <duniaalkilany908@gmail.com>
2021-07-04 21:48:34 +0300

    finished lab 11 front-end

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Footer from './Footer';
 import Profile from './Profile';
 import BestBooks from './BestBooks'
 import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -14,30 +16,39 @@ import {
 } from "react-router-dom";
 
 class App extends React.Component {
-
+  componentDidMount = () => {
+    if(this.props.auth0.isAuthenticated) {
+      this.props.auth0.getIdTokenClaims()
+      .then(res => {
+        const jwt = res.__raw;
+        const config = {
+          headers: {"Authorization" : `Bearer ${jwt}`},
+          method: 'get',
+          baseURL: process.env.REACT_APP_SERVER_URL,
+          url: '/authorize'
+        }
+        axios(config)
+          .then(axiosResults => console.log(axiosResults.data))
+          .catch(err => console.error(err));
+      })
+      .catch(err => console.error(err));
+    }
+  }
   render() {
+    const { user, isAuthenticated } = this.props.auth0;
     console.log(this.props.auth0)
+    console.log(user);
     return(
       <>
         <Router>
           <IsLoadingAndError>
-            <Header />
+            <Header isAuthenticated={isAuthenticated} />
             <Switch>
-            {
-              this.props.auth0.isAuthenticated ?
-              <>
-              <BestBooks/>
-             
-            <Profile />
-           
-          
-              </>:
-              <>
-              <Route exact path="/login">
-              <Login/>
+            <Route exact path="/">{isAuthenticated ? <BestBooks /> : <Login />}
               </Route>
-              </>}
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Route exact path="/profile">{isAuthenticated ? <Profile userInfo={user} /> : ''} 
+              </Route>
+      
             </Switch>
             <Footer />
           </IsLoadingAndError>
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import './Header.css';
-
+import LogoutButton from './LogoutButton';
+// import { withAuth0 } from '@auth0/auth0-react';
 class Header extends React.Component {
   render() {
     return(
@@ -11,6 +12,11 @@ class Header extends React.Component {
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
+        {this.props.isAuthenticated ? <LogoutButton /> : ''}
+{/* 
+        {
+              this.props.auth0.isAuthenticated ?
+              <LogoutButton/>:""} */}
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
       </Navbar>
     );
